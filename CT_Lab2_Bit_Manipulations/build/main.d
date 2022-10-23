a4e043914a7f88395595900da1f2a37656721168
Armando (shalaar3) <shalaar3@students.zhaw.ch>
2022-10-23 13:10:04 +0200

    Lab2 Bit Manipulations done (leeched)

--- CT_Lab2_Bit_Manipulations/build/main.d
+++ CT_Lab2_Bit_Manipulations/build/main.d
@@ -1,2 +1,5 @@
 ./build/main.o: app\main.c app\utils_ctboard.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Users\armandoshala\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.2\Device\Include\reg_ctboard.h
